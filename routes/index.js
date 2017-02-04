59a9bb9d8621fe1e8167d84dacf42b51783f0417
Franco Chong <fnbeddie@hotmail.com>
2017-02-03 22:40:56 -0500

    added more pages

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,6 +9,21 @@ router.get('/', function(req, res, next) {
 //get about page
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About Me' });
+});
+
+//get projects page
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'My Projects' });
+});
+
+//get service page
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'Services' });
+});
+
+//get contact page
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Me' });
 });
 
 module.exports = router;
