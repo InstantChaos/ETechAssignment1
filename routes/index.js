8a5dd45773d08b6015f804db2c96491c2fa8b8a0
Franco Chong <fnbeddie@hotmail.com>
2017-02-03 22:23:10 -0500

    added pages

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home Page' });
+});
+
+//get about page
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Me' });
 });
 
 module.exports = router;
